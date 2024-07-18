485ae24e7eaaa214b73409fa9d3fdb36b70374cb
DerpNPurp <vincedoanschool@gmail.com>
2024-07-18 16:11:56 -0700

    Added Express.js, Socket.io, broken drawing canvas

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+//Socket.io setup || Socket.io server wrapped around
+// an http server wrapped around an express server
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const port = 3000
+
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
